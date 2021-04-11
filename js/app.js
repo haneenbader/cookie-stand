689a17afa9f18eb3043ce9ff066f936af09e31da
haneenbader <haneenalwatan993@gmail.com>
2021-04-12 00:41:19 +0300

    added dom for seatle

--- js/app.js
+++ js/app.js
@@ -20,3 +20,40 @@ const seattle = {
 };
 seattle.avgCust();
 
+let container=document.getElementById('container');
+let locationName=document.createElement('h1');
+container.appendChild(locationName);
+locationName.textContent='Seattle';
+
+
+let unorderedList=document.createElement('ul');
+container.appendChild(unorderedList);
+
+let amPm='';
+let list='';
+let total=0;
+let clock=5;
+
+
+for(let i =0 ; i<15 ; i++){
+  list=document.createElement('li');
+  unorderedList.appendChild(list);
+  if (clock < 12 ){
+    clock++;
+  }else{
+    clock=1;
+  }if(clock < 12 && i<6){
+    amPm='am';
+  }
+  if(i>=6){
+    amPm='pm';
+  }
+  if(i<14){
+    list.textContent=`${clock}${amPm}: ${seattle.cookPurchEachHr[i]}`;
+    total=total+seattle.cookPurchEachHr[i];
+  }else{
+    list.textContent=`total:${total}`;
+  }
+}
+
+
