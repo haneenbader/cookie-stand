6785c133d4e18cb62ce14bb9247e290ef7b66551
haneenbader <haneenalwatan993@gmail.com>
2021-04-12 00:56:54 +0300

    added object and dom to tokyo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-//seattle
 const seattle = {
   minCust:23,
   maxCust:65,
@@ -20,20 +19,19 @@ const seattle = {
 };
 seattle.avgCust();
 
+
 let container=document.getElementById('container');
 let locationName=document.createElement('h1');
 container.appendChild(locationName);
 locationName.textContent='Seattle';
 
-
 let unorderedList=document.createElement('ul');
 container.appendChild(unorderedList);
 
-let amPm='';
-let list='';
-let total=0;
 let clock=5;
-
+let list='';
+let amPm='';
+let total=0;
 
 for(let i =0 ; i<15 ; i++){
   list=document.createElement('li');
@@ -42,7 +40,8 @@ for(let i =0 ; i<15 ; i++){
     clock++;
   }else{
     clock=1;
-  }if(clock < 12 && i<6){
+  }
+  if(clock < 12 && i<6){
     amPm='am';
   }
   if(i>=6){
@@ -56,4 +55,59 @@ for(let i =0 ; i<15 ; i++){
   }
 }
 
+
+const Tokyo = {
+  minCust:3,
+  maxCust:24,
+  avgCookie:1.2,
+  avgCustPerHr:[],
+  cookPurchEachHr:[],
+  rand:function () {
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  },
+  avgCust: function(){
+    for(let i=0 ; i < 14 ; i++){
+      this.avgCustPerHr[i]=this.rand();
+      this.cookPurchEachHr[i]=Math.floor(this.avgCustPerHr[i]*this.avgCookie);
+    }
+  },
+
+};
+Tokyo.avgCust();
+
+
+container=document.getElementById('container');
+locationName=document.createElement('h1');
+container.appendChild(locationName);
+locationName.textContent='tokyo';
+
+unorderedList=document.createElement('ul');
+container.appendChild(unorderedList);
+
+clock=5;
+list='';
+amPm='';
+total=0;
+
+for(let i =0 ; i<15 ; i++){
+  list=document.createElement('li');
+  unorderedList.appendChild(list);
+  if (clock < 12 ){
+    clock++;
+  }else{
+    clock=1;
+  }
+  if(clock < 12 && i<6){
+    amPm='am';
+  }
+  if(i>=6){
+    amPm='pm';
+  }
+  if(i<14){
+    list.textContent=`${clock}${amPm}: ${seattle.cookPurchEachHr[i]}`;
+    total=total+Tokyo.cookPurchEachHr[i];
+  }else{
+    list.textContent=`total:${total}`;
+  }
+}
 
