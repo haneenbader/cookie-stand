34d54a6182872bee04246065a6644ab414ecdbf0
haneenbader <haneenalwatan993@gmail.com>
2021-04-12 01:01:10 +0300

    added object and dom to Dubai location

--- js/app.js
+++ js/app.js
@@ -111,3 +111,59 @@ for(let i =0 ; i<15 ; i++){
   }
 }
 
+
+const Dubai = {
+  minCust:11,
+  maxCust:38,
+  avgCookie:3.7,
+  avgCustPerHr:[],
+  cookPurchEachHr:[],
+  rand:function () {
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  },
+  avgCust: function(){
+    for(let i=0 ; i < 14 ; i++){
+      this.avgCustPerHr[i]=this.rand();
+      this.cookPurchEachHr[i]=Math.floor(this.avgCustPerHr[i]*this.avgCookie);
+    }
+  },
+
+};
+Dubai.avgCust();
+
+
+container=document.getElementById('container');
+locationName=document.createElement('h1');
+container.appendChild(locationName);
+locationName.textContent='Dubai';
+
+unorderedList=document.createElement('ul');
+container.appendChild(unorderedList);
+
+clock=5;
+list='';
+amPm='';
+total=0;
+
+for(let i =0 ; i<15 ; i++){
+  list=document.createElement('li');
+  unorderedList.appendChild(list);
+  if (clock < 12 ){
+    clock++;
+  }else{
+    clock=1;
+  }
+  if(clock < 12 && i<6){
+    amPm='am';
+  }
+  if(i>=6){
+    amPm='pm';
+  }
+  if(i<14){
+    list.textContent=`${clock}${amPm}: ${seattle.cookPurchEachHr[i]}`;
+    total=total+Dubai.cookPurchEachHr[i];
+  }else{
+    list.textContent=`total:${total}`;
+  }
+}
+
